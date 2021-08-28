bb4bff18b59b69527850bde33e1e1839f4c2830d
Shiva Prasad <shivapv@pm.me>
2021-08-28 15:18:32 +0530

    Give life to repl: read, print and error feedback

--- repl.c
+++ repl.c
@@ -1,12 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <editline/readline.h>
+#include "io.c"
+enum err_code {
+	UNEXP_TOKEN, INCOMPL_EXP,
+	TRAIL_CHARS, EMPTY_STR
+};
+
+void printerr(unsigned pad, enum err_code err)
+{
+	static const char arrow[] = "^--";
+	static const char *err_msgs[] = {
+		"Unexpected token",
+		"Incomplete expression",
+		"Trailing characters"
+	};
+	if (err != EMPTY_STR) {
+		while (pad--)
+			putchar(' ');
+		printf("  %s %s\n", arrow, err_msgs[err]);
+	}
+}
+
+Cell *read(char s[])
+{
+	Cell *cp;
+	int slen, rlen;
+	enum err_code err;
+	cp = new_cell(NULL);
+	slen = strlen(s);
+	if ((rlen = readexp(cp, s)) < 0) {
+		rlen = -rlen;
+		err = (rlen == slen) ? INCOMPL_EXP : UNEXP_TOKEN;
+	} else if (rlen == 0) {
+		err = slen ? UNEXP_TOKEN : EMPTY_STR;
+	} else if (rlen < slen) {
+		err = TRAIL_CHARS;
+	} else {
+		return cp;
+	}
+	printerr(rlen, err);
+	return NULL;
+}
+
+void print(Cell *cp)
+{
+	if (cp) {
+		printexp(cp);
+		putchar('\n');
+	}
+}
 
 int main()
 {
 	char *s;
 	while (s = readline("> ")) {
-		puts(s);
+		print(read(s));
 		add_history(s);
 		free(s);
 	}
